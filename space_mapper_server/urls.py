5beb8090c7dee2e326321773314b762fb9037087
John Palmer <johnrbpalmer@gmail.com>
2022-05-18 12:19:34 +0200

    changed url for getting user code

--- space_mapper_server/urls.py
+++ space_mapper_server/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     re_path(r'^api/', include('tracks.urls')),
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'^mobility_mapper_code/', show_user_code, name='show_user_code'),
+    re_path(r'^space_mapper_code/', show_user_code, name='show_user_code'),
     re_path(r'^fix_stats/', download_fix_stats, name='download_fix_stats'),
     re_path(r'^reg_stats/', download_reg_stats, name='download_reg_stats'),
     re_path(r'^accounts/login/$', auth.views.LoginView.as_view(), name='auth_login'),
