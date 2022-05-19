b20ef02295210196219dfad2eefb951015da74fe
John Palmer <johnrbpalmer@gmail.com>
2022-05-19 12:14:28 +0200

    fixed issue with admin site field names

--- tracks/admin.py
+++ tracks/admin.py
@@ -3,9 +3,9 @@ from tracks.models import *
 
 
 class DataPointAdmin(admin.ModelAdmin):
-    list_display = ('user_UUID', 'user_code',  'app_version', 'type', 'last_modified', 'created',)
-    readonly_fields = ('user_UUID', 'user_code', 'app_version','type', 'encrypted_message', 'last_modified', 'created',)
-    fields = ('user_UUID', 'user_code', 'app_version', 'type', 'encrypted_message', 'last_modified', 'created',)
+    list_display = ('user_UUID', 'user_code',  'app_version', 'type_of_data', 'last_modified', 'created',)
+    readonly_fields = ('user_UUID', 'user_code', 'os', 'app_version','type_of_data', 'encrypted_message', 'encrypted_lon', 'encrypted_lat', 'encrypted_unix_time', 'encrypted_speed', 'encrypted_altitude', 'encrypted_activity', 'message', 'lon', 'lat', 'unix_time', 'speed', 'altitude', 'activity', 'last_modified', 'created',)
+    fields = ('user_UUID', 'user_code', 'os', 'app_version', 'type_of_data', 'encrypted_message', 'encrypted_lon', 'encrypted_lat', 'encrypted_unix_time', 'encrypted_speed', 'encrypted_altitude', 'encrypted_activity', 'message', 'lon', 'lat', 'unix_time', 'speed', 'altitude', 'activity', 'last_modified', 'created',)
 
     def has_add_permission(self, request):
         return False
